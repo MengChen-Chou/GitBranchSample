d8fc9f9c470be8a9b38034deebe69c3ac8d37a62
RickChou <chou830905@gmail.com>
2023-01-07 14:12:48 +0800

    add two

--- GitBranchTest/src/test1/MainApp.java
+++ GitBranchTest/src/test1/MainApp.java
@@ -4,5 +4,14 @@ public class MainApp {
 	public static void main(String[] args) {
 		//組長
 		System.out.println("1st version");
+		//組長新增
+		Test cMath = new Test();
+		System.out.println(cMath.add(10, 20));
+	}
+}
+
+class Test{
+	int add(int a, int b) {
+		return a+b;
 	}
 }
